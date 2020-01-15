b7986f94ea8bff08d07cf096d6bbc66e15fb0443
Jakub Antczak <jakubantczak.info@gmail.com>
2020-01-15 13:21:11 +0100

    Refactor the app only with EJS

--- views/includes/head.js
+++ views/includes/head.js
@@ -5,13 +5,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add product</title>
-</head>
-
-<body>
-    <header>
-        <h5>Page not found!</h5>
-    </header>
-</body>
-
-</html>
+    <title><%= pageTitle %></title>
+    <link rel="stylesheet" href="/css/main.css"></link>
