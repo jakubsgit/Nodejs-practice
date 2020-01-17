bbc6005b4e8c00ab0d79dbbcaef7cb93d79cefd4
Jakub Antczak <jakubantczak.info@gmail.com>
2020-01-17 10:15:06 +0100

    Added MySQL DataBase to our project

--- Node.js
+++ Node.js
@@ -6,12 +6,17 @@ const express = require("express");
 const app = express();
 
 const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRouter = require("./routes/shop");
+
+db.execute("SELECT * FROM products")
+  .then(result => console.log(result))
+  .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
