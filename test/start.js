723f749e1350592e869f51c080904bedd1488050
Jakub Antczak <jakubantczak.info@gmail.com>
2020-02-04 11:11:03 +0100

    before testing projects  middleware

---
+++ test/start.js
@@ -0,0 +1,14 @@
+const expect = require("chai").expect;
+
+it("should add numbers correctly", function() {
+  const num1 = 2;
+  const num2 = 3;
+  const result = +num1 + +num2;
+  expect(result).to.equal(5);
+});
+it("should add numbers correctly", function() {
+  const num1 = 2;
+  const num2 = 3;
+  const result = +num1 + +num2;
+  expect(result).to.equal(5);
+});
